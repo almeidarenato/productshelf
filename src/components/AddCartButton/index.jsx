a793076154238b56fa95d9b675b2a8f45f6d3cae
Renato Almeida <renatoam@ymail.com>
2023-08-09 20:45:23 -0300

    feat: criação do botao wishlist

---
+++ src/components/AddCartButton/index.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./AddCartButton.css";
+const AddCartButton = () => {
+  const [addToCart, setAddToCart] = useState(false);
+  const handleAddToCart = () => {
+    setAddToCart(true);
+    console.log(addToCart);
+  };
+  return (
+    <button
+      className={"addCartButton" + (addToCart ? " addCartButton--active" : "")}
+      onClick={() => handleAddToCart()}
+    >
+      {addToCart ? "Adicionado" : "Adicionar"}
+    </button>
+  );
+};
+
+export default AddCartButton;
